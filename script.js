0e6adfcb581dc48ad31dfc3a63658ff86c8ef3dc
Conrad Etherington <conrad.etherington@gmail.com>
2022-10-01 00:48:09 -0600

    added div with id of main. added style rules to css to change div size and set margins and color. added message to div in html

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const videoMessage = document.createElement(div);
+videoMessage.textContent = "Added this to main div with java script. this div uses css to have a different colors, so thats cool. Anyways Im gonna link a picture in this div for you to see."
+document.querySelector("body").appendChild(div)
